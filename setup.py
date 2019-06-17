7062c41a8a36cdb506bb0943939d2b7efd725fda
BHARATHKUMAR SIVAKUMAR <35873425+ITSMESBK@users.noreply.github.com>
2019-06-18 00:23:51 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'BharathkumarSivakumar',                   # Type in your name
   author_email = 'sivabharath70@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ITSMESBK/add/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ITSMESBK/bharath_add/archive/master.zip',    # I explain this later on
   keywords = ['add', 'addition', 'sum'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
